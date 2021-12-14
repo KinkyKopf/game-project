a96f961ef6bab9afe1761f1a547f8881e25e4ac9
KinkyKopf <95887857+KinkyKopf@users.noreply.github.com>
2021-12-14 14:21:33 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -9,10 +9,11 @@ public class Inventory
 	
 	/*
 	 * Notes:
-	 * I am going to need to make each possible object have a specific value and then have something that  can make use of said values
+	 * I am going to need to make each possible object have a specific value and then have something that can make use of said values
 	 */
 	private int gold,potions,upgradeTokens;
-	String weapon;
+	private String weaponName;
+	 Object[] inventorySlots=new Object[1];
 	
 	public Inventory(int g,int u, String w)
 	{
@@ -25,7 +26,8 @@ public class Inventory
 		this(1000,2,"Gauntlets");
 	}
 	
-	//getters
+	//getters_____________
+	
 	public int getGold()
 	{
 		return gold;
@@ -36,12 +38,16 @@ public class Inventory
 	}
 	public String getWeapon()
 	{
-		return weapon;
+		return weaponName;
 	}
 	//Setters____________________
+	public void setArray(Object weapon)
+	{
+		inventorySlots[0]=weapon;
+	}
 	public void setWeapon(String s)
 	{
-		weapon=s;
+		weaponName=s;
 	}
 	public void setGold(int g)
 	{
@@ -60,11 +66,11 @@ public class Inventory
 		upgradeTokens+=adder;
 	}
 	//Miscelanious________________________
-	
-		/*
-		 * add protecion
-		 * 
-		 */
+	public String toString()
+	{
+		return "Gold: "+gold+"\nUpgrade Tokens: "+upgradeTokens+"\nWeapon: "+weaponName;
+	}
+		
 		public class Sword//WIP(duh)
 		{
 			private int minDamage,maxDamage,damageBonus,protection;
@@ -74,8 +80,17 @@ public class Inventory
 				setMin(1);
 				setMax(4);
 				setProtection(2);
+				
+			}
+			public Sword(int min,int max,int p,boolean suped)
+			{
+				setMin(min);
+				setMax(max);
+				setProtection(p);
+				enchanted =suped;
 			}
 			
+			//setters_____________________________
 			public void addDamage(int dam)//bonus damage will be for upgrading the weapon
 			{
 				damageBonus+=dam;
@@ -93,6 +108,31 @@ public class Inventory
 				protection= p;
 			}
 			
+			//Getters__________________________
+			public int getDamage()
+			{
+				return damageBonus;
+			}
+			public int getMin()
+			{
+				return minDamage;
+			}
+			public int getMax()
+			{
+				return maxDamage;
+			}
+			public int getProtection()
+			{
+				return protection;
+			}
+			public boolean enchanted()
+			{
+				return enchanted;
+			}
+			
+			//Misc methods___________________________________
+			
+			
 			public int randomGen(int min, int max)
 			{
 				return (int)(Math.random()*(max-min+1))+min;
@@ -104,6 +144,90 @@ public class Inventory
 				if(enchanted)
 					return 2*damage;
 				return damage;
+			}
+			public String toString()
+			{
+				return "Damage: "+damageBonus+"\nProtection: "+"\nEnchanted: "+enchanted;
+			}
+			
+		}
+		public class Shield//WIP(duh)
+		{
+			private int minDamage,maxDamage,damageBonus,protection;
+			private boolean enchanted;
+			public Shield()//constructor
+			{
+				setMin(1);
+				setMax(4);
+				setProtection(2);
+				inventorySlots[0]=2;
+			}
+			public Shield(int min,int max,int p,boolean suped)
+			{
+				setMin(min);
+				setMax(max);
+				setProtection(p);
+				enchanted =suped;
+			}
+			
+			//setters_____________________________
+			public void addDamage(int dam)//bonus damage will be for upgrading the weapon
+			{
+				damageBonus+=dam;
+			}
+			public void setMin(int min)
+			{
+				minDamage=min;
+			}
+			public void setMax(int max)
+			{
+				maxDamage=max;
+			}
+			public void setProtection(int p)
+			{
+				protection= p;
+			}
+			
+			//Getters__________________________
+			public int getDamage()
+			{
+				return damageBonus;
+			}
+			public int getMin()
+			{
+				return minDamage;
+			}
+			public int getMax()
+			{
+				return maxDamage;
+			}
+			public int getProtection()
+			{
+				return protection;
+			}
+			public boolean enchanted()
+			{
+				return enchanted;
+			}
+			
+			//Misc methods___________________________________
+			
+			
+			public int randomGen(int min, int max)
+			{
+				return (int)(Math.random()*(max-min+1))+min;
+			}
+			public int rollDamage()
+			{
+				
+				int damage= randomGen(minDamage,maxDamage)+damageBonus;
+				if(enchanted)
+					return 2*damage;
+				return damage;
+			}
+			public String toString()
+			{
+				return "Damage: "+damageBonus+"\nProtection: "+"\nEnchanted: "+enchanted;
 			}
 			
 		}
