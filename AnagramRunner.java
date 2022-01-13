cf8709955c83d3ccc0c65acb29909dd09f472336
KinkyKopf <95887857+KinkyKopf@users.noreply.github.com>
2022-01-12 22:35:08 -0500

    Add files via upload

---
+++ AnagramRunner.java
@@ -0,0 +1,11 @@
+package unit5;
+
+public class AnagramRunner {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Anagram.getRidOfStupidStuff("s w 'd"));
+		System.out.println(Anagram.testAnagrams("word ", "   wood"));
+	}
+
+}
